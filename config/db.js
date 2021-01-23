5e0f7c8ba37dfe6786a7f2ac376e0d8f1558e564
Kalp Kumar Varshney <kalpvars2@gmail.com>
2021-01-23 19:36:39 +0530

    readded process.env.DB_URL

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try{
-		await mongoose.connect(process.env.MONGODB_URI, {
+		await mongoose.connect(process.env.MONGODB_URI || process.env.DB_URL, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useFindAndModify: true,
