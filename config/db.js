64f603ff36fbef2fc4147f336c349093fa22bbba
Kalp Kumar Varshney <kalpvars2@gmail.com>
2021-01-23 19:32:30 +0530

    removed process.env.db_url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
 	try{
-		await mongoose.connect(process.env.MONGODB_URI || process.env.DB_URL, {
+		await mongoose.connect(process.env.MONGODB_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			useFindAndModify: true,
